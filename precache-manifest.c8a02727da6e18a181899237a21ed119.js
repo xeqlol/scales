170398371fdd20cfe53ae4b8333c14dd5a955db3
Dimitrii Nemkov <xeqlol@gmail.com>
2020-01-27 22:39:44 +0500

    Updates

---
+++ precache-manifest.c8a02727da6e18a181899237a21ed119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9870f5fb58fc5b322a6ef9dfdb7a5eba",
+    "url": "/scales/index.html"
+  },
+  {
+    "revision": "3062a2bf2e2da8e9ebe1",
+    "url": "/scales/static/css/main.44978faf.chunk.css"
+  },
+  {
+    "revision": "f13f16de7b2a70f35a2d",
+    "url": "/scales/static/js/2.8a1a3f8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scales/static/js/2.8a1a3f8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3062a2bf2e2da8e9ebe1",
+    "url": "/scales/static/js/main.91f31db2.chunk.js"
+  },
+  {
+    "revision": "e71c27eadd9c0c07d725",
+    "url": "/scales/static/js/runtime-main.5d8d3be8.js"
+  }
+]);
