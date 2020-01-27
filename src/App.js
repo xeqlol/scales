7e4361b8aa5c0ae7096c3040d97f6d991b8064ab
Dimitrii Nemkov <xeqlol@gmail.com>
2020-01-27 22:32:18 +0500

    feat: added scale and root selectors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   NOTES_COUNT,
+  SCALES,
+  NOTES,
   filterNotesByScale,
   getNotesForFrets,
   getLabelForNote
@@ -12,28 +14,50 @@ import "./App.css";
 const TUNING = [4, 11, 7, 2, 9, 4];
 const ROOT = 4;
 const FRETS_COUNT = 24;
-const PATTERN = [0, 2, 3, 5, 7, 8, 11]; // harmonic minor
 
 const App = () => {
+  const [scale, setScale] = useState(0);
+  const [root, setRoot] = useState(0);
+
+  const { pattern } = SCALES[scale];
+
   const frets = TUNING.map(startNote =>
     filterNotesByScale(
       getNotesForFrets(startNote, FRETS_COUNT),
-      PATTERN.map(note => (note + ROOT) % (NOTES_COUNT - 1))
+      pattern.map(note => (note + root) % (NOTES_COUNT - 1))
     ).map(getLabelForNote)
   );
-  console.log(frets);
+  console.log(scale, root);
   return (
-    <div className={"frets"}>
-      {frets[0].map((_, fretIndex) => (
-        <div className={"fret"}>
-          {frets.map((_, stringIndex) => (
-            <div className={"note"}>
-              <div className={"label"}>{frets[stringIndex][fretIndex]}</div>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <select
+        value={scale}
+        onChange={event => setScale(Number(event.target.value))}
+      >
+        {SCALES.map(({ name }, index) => (
+          <option value={index}>{name}</option>
+        ))}
+      </select>
+      <select
+        value={root}
+        onChange={event => setRoot(Number(event.target.value))}
+      >
+        {NOTES.map((note, index) => (
+          <option value={index}>{note}</option>
+        ))}
+      </select>
+      <div className={"frets"}>
+        {frets[0].map((_, fretIndex) => (
+          <div className={"fret"}>
+            {frets.map((_, stringIndex) => (
+              <div className={"note"}>
+                <div className={"label"}>{frets[stringIndex][fretIndex]}</div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const NOTES = [
+export const NOTES = [
   "C",
   "C#",
   "D",
@@ -15,6 +15,20 @@ const NOTES = [
 ];
 
 export const NOTES_COUNT = NOTES.length;
+
+const C_HARMONIC_MINOR = [0, 2, 3, 5, 7, 8, 11];
+const C_MINOR = [0, 2, 3, 5, 7, 8, 10];
+
+export const SCALES = [
+  {
+    name: "Minor",
+    pattern: C_MINOR
+  },
+  {
+    name: "Harmonic minor",
+    pattern: C_HARMONIC_MINOR
+  }
+];
 
 export const getLabelForNote = noteIndex => {
   return NOTES[noteIndex];
