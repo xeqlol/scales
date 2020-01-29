45d12bfde053063a4d3f35aadcc947a770ec2f43
xeqlol <xeqlol@yandex-team.ru>
2020-01-29 19:00:41 +0500

    feat: added diminished scale

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -40,6 +40,10 @@ export const SCALES = [
   {
     name: "Melodic major",
     pattern: [0, 2, 4, 5, 7, 8, 10]
+  },
+  {
+    name: "Diminished",
+    pattern: [0, 2, 3, 5, 6, 8, 9, 11]
   }
 ];
 
